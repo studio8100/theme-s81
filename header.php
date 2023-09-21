8250187be64ed3e48e2096c9a34646a66c4cebdf
Studio8100 <studio8100@yahoo.com>
2023-09-21 11:44:25 -0400

    testing push change to github, non-important commit

--- header.php
+++ header.php
@@ -4,6 +4,6 @@
     <?php wp_head(); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Theme S81</title>
 </head>
 <body>
